4371d9bf9dded353e801984f633d5c74326935ca
Shikha sharma <135316685+Shikhasharma06@users.noreply.github.com>
2023-12-17 21:16:53 +0530

    Create footer.js

---
+++ component/style/footer.js
@@ -0,0 +1,15 @@
+import './Style/footer.css';
+
+
+function Footer(){
+    return (
+        <>
+        
+        <footer className="footer">
+        Copyright 2023 | All Rights Are reserved | Made By Aadarsh Raj |The Arya Solution
+        </footer>
+        </>
+    );
+}
+
+export default Footer;
